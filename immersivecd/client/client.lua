81c5bb11c43d7504d572e5e6d59fa9cf895addd3
quantum <132041342+quantumwrks@users.noreply.github.com>
2024-02-24 15:27:04 +0200

    Add files via upload

---
+++ immersivecd/client/client.lua
@@ -0,0 +1,2 @@
+-- Make the ped NOT lose their props when they're hit or pushed.
+SetPedCanLosePropsOnDamage(PlayerPedId(), false, 0)
---
+++ immersivecd/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+version '1.0.3'
+lua54 'yes'
+
+client_scripts {
+    'client/client.lua',
+}
